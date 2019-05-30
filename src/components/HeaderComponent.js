9225253b9fce03d5df824ee741cec5ca0de49973
Drithyl <hugo.bujan@gmail.com>
2019-05-30 12:14:17 -0400

    React Uncontrolled Forms

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from "react";
-import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron } from "reactstrap";
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label } from "reactstrap";
 import { NavLink } from "react-router-dom";
 
 class Header extends Component
@@ -9,15 +9,31 @@ class Header extends Component
   {
     super(props);
     this.state = {
-      isNavOpen: false
+      isNavOpen: false,
+      isModalOpen: false
     };
 
     this.toggleNav = this.toggleNav.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
   }
 
   toggleNav()
   {
     this.setState({ isNavOpen: !this.state.isNavOpen });
+  }
+
+  toggleModal()
+  {
+    this.setState({ isModalOpen: !this.state.isModalOpen });
+  }
+
+  handleLogin(event)
+  {
+    this.toggleModal();
+    //values are retrieved directly from the DOM thanks to the innerRef set up in the form below
+    alert(`Username ${this.username.value} Password: ${this.password.value} Remember: ${this.remember.checked}`);
+    event.preventDefault();
   }
 
   render()
@@ -82,6 +98,14 @@ class Header extends Component
                   </NavLink>
                 </NavItem>
               </Nav>
+
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <Button outline onClick={this.toggleModal}>
+                    <span className="fa fa-sign-in fa-lg"></span> Login
+                  </Button>
+                </NavItem>
+              </Nav>
             </Collapse>
           </div>
         </Navbar>
@@ -91,11 +115,43 @@ class Header extends Component
             <div className="row row-header">
               <div className="col-12 col-sm-6">
                 <h1>Ristorante Con Fusion</h1>
-                <p>We take inspiration from from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
+                <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
               </div>
             </div>
           </div>
         </Jumbotron>
+
+        {/* Reacstrap Modal requires the isOpen attribute and the toggle function */}
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader>Login</ModalHeader>
+
+          <ModalBody>
+            <Form onSubmit={this.handleLogin}>
+              <FormGroup>
+                <Label htmlFor="username">Username</Label>
+
+                {/* innerRef is like a ref but reactstrap already uses that term
+                    for something else, hence the different name*/}
+                <Input type="text" id="username" name="username" innerRef={(input) => this.username = input}/>
+              </FormGroup>
+
+              <FormGroup>
+                <Label htmlFor="password">Password</Label>
+
+                <Input type="password" id="password" name="password" innerRef={(input) => this.password = input}/>
+              </FormGroup>
+
+              <FormGroup check>
+                <Label check>
+                  <Input type="checkbox" name="remember" innerRef={(input) => this.remember = input}/>
+                  Remember me
+                </Label>
+              </FormGroup>
+
+              <Button type="submit" value="submit" color="primary">Login</Button>
+            </Form>
+          </ModalBody>
+        </Modal>
       </React.Fragment>
     );
   }
